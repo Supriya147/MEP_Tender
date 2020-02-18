c0585012bc062e281268d8195469e56f1ee48a21
Supriya Gaikwad <supriyagaikwad111@outlook.com>
2020-02-18 09:04:03 +0400

    Create RegisterEmployee.cshtml.cs

---
+++ MEPTender/Pages/RegisterEmployee.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MEPTender.Models;
+
+namespace MEPTender.Pages
+{
+    public class RegisterEmployee : PageModel
+    {
+        DAL objemployee = new DAL();
+
+        [BindProperty]
+        public Employee employee { get; set; }
+
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            objemployee.AddEmployee(employee);
+
+            return RedirectToPage("./RegisterEmployee");
+        }
+    }
+}
