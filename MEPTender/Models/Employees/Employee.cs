5c51956918fd6f2a50c98940c9a2edd28cc46b73
Supriya Gaikwad <supriyagaikwad111@outlook.com>
2020-02-18 09:00:40 +0400

    Create Employee.cs

---
+++ MEPTender/Models/Employees/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MEPTender.Models
+{
+    public class Employee
+    {
+        public string ProjectDetails { get; set; }
+        [Required]
+        public int Client { get; set; }
+        [Required]
+        public int Consultant { get; set; }
+        [Required]
+        public int Contractor { get; set; }
+
+    }
+
+    public class Client
+    {
+        public int Client_ID { get; set; }
+        public string ClientName { get; set; }
+    }
+
+    public class Consultant
+    {
+        public int Consultant_ID { get; set; }
+        public string ConsultantName { get; set; }
+    }
+
+    public class Contractor
+    {
+        public int Contractor_ID { get; set; }
+        public string ContractorName { get; set; }
+    }
+}
